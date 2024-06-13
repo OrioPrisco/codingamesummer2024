d9800cacb267ca84c4e6242b27943578ff649439
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-06-14 01:58:29 +0200

    add parameter.hpp
    oops

---
+++ includes/parameters.hpp
@@ -0,0 +1,12 @@
+#pragma once
+
+// POP_ME >= POP_OPP !!!
+#define POP_ME 50
+#define POP_OPP 10
+#define DEFAULT_MUTATION_RATE 50
+#define MIN_MUTATION_RATE 5
+#define GEN_TO_LOWER_MUTATION_RATE 3
+
+#include <cstdint>
+typedef uint64_t Strat;
+#define MOVE_PER_STRAT 32
