38152ce7fcdbfe1eda290280343b0c2a663c8207
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-06-14 13:38:28 +0200

    uint64_t -> Strat

--- sources/main.cpp
+++ sources/main.cpp
@@ -114,7 +114,7 @@ Strat optimal_runner(const std::string& gpu, size_t pos, int stun) {
 
 typedef Strat Strats[POP_ME];
 void evolve_strats(const MiniGame (&games)[4], Strats (&strats)[3] , int player, int percent_mutation, int population_size) {
-	std::multimap<double, uint64_t, std::greater<double>> ranked_strats;
+	std::multimap<double, Strat, std::greater<double>> ranked_strats;
 	int opp1 = (player + 1) % 3;
 	int opp2 = (player + 2) % 3;
 
