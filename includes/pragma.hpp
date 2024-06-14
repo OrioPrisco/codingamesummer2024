3dc07716b56c0454d32d8f80f3c7369fcd4c2640
OrioPrisco <47635210+OrioPrisco@users.noreply.github.com>
2024-06-14 19:34:45 +0200

    Pragmas galore
    TOP 15 SILVER WOOOO

--- includes/pragma.hpp
+++ includes/pragma.hpp
@@ -1,4 +1,12 @@
+// trigger optimisation from source file
 #pragma GCC optimize("O3")
 #pragma GCC optimize("inline")
 #pragma GCC optimize("omit-frame-pointer")
-//#pragma GCC optimize("unroll-loops")
+#pragma GCC optimize("unroll-loops") //Optimization flags
+
+#pragma GCC optimize("Ofast")
+
+#pragma GCC option("arch=native","tune=native","no-zero-upper") //Enable AVX
+#pragma GCC target("avx")  //Enable AVX
+#pragma GCC target "bmi2"
+#include <x86intrin.h> //AVX/SSE Extensions
